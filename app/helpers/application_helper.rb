133109d4275013f5403b4e87fbf571f07d3d8364
Via-R <viar99criminal@gmail.com>
2018-02-23 22:36:31 +0200

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#Returns correct title for pages if it's not provided
+	def full_title(page_title = '')
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_title
+		else
+			page_title + ' | ' + base_title
+		end
+	end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,19 +3,18 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
-    @index_name = "Home"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", @base_title
   end
 
   test "should get index" do
     get "/"
     assert_response :success
-    assert_select "title", "#{@index_name} | #{@base_title}"
+    assert_select "title", @base_title
   end
 
   test "should get help" do
